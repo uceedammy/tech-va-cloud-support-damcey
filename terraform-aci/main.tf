acefa6097c21a1c3033056d3626bc34b45026a08
Damilola Uche-Lynda <Damilola_Uche@UCHE.damceyafrique.com>
2025-05-04 00:37:35 +0100

    deploy to ACI

--- terraform-aci/main.tf
+++ terraform-aci/main.tf
@@ -1,12 +1,31 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 resource "random_integer" "suffix" {
   min = 10000
   max = 99999
 }
 
+resource "azurerm_resource_group" "main" {
+  name     = "damcey-rg"
+  location = "eastus"
+}
+
+
 resource "azurerm_container_group" "app" {
   name                = "damceyapp-${random_integer.suffix.result}"
-  location            = "eastus"
-  resource_group_name = "damcey-rg"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
   ip_address_type     = "Public"
   dns_name_label      = "damceyapp-${random_integer.suffix.result}"
   os_type             = "Linux"
