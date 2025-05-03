46de171f24e0e455f22c5b9493e740f3884988dc
Damilola Uche-Lynda <Damilola_Uche@UCHE.damceyafrique.com>
2025-05-03 23:50:02 +0100

    build docker image and deploy to ACI

--- terraform-aci/main.tf
+++ terraform-aci/main.tf
@@ -1,13 +1,25 @@
+variable "ghcr_token" {}
+variable "ghcr_image_url" {}
+variable "resource_group_name" {
+  default = "damcey-terraform-rg"
+}
+variable "location" {
+  default = "eastus"
+}
+variable "dns_name_label" {
+  default = "damceyapp-${replace(uuid(), "-", "")}"
+}
+
 resource "azurerm_container_group" "app" {
-  name                = "damceyapp-${random_integer.suffix.result}"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  name                = "damceyapp"
+  location            = var.location
+  resource_group_name = var.resource_group_name
   os_type             = "Linux"
   ip_address_type     = "Public"
-  dns_name_label      = "damceyapp-${random_integer.suffix.result}"
+  dns_name_label      = var.dns_name_label
 
   container {
-    name   = "app"
+    name   = "damcey-app"
     image  = var.ghcr_image_url
     cpu    = "0.5"
     memory = "1.5"
