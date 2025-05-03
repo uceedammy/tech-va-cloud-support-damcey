98f22e30011ae9b62058b39de0bcfd148d9a76bf
Damilola Uche-Lynda <Damilola_Uche@UCHE.damceyafrique.com>
2025-05-03 23:59:49 +0100

    build docker image and deploy to ACI

--- terraform-aci/main.tf
+++ terraform-aci/main.tf
@@ -1,22 +1,22 @@
-variable "ghcr_token" {}
-variable "ghcr_image_url" {}
-variable "resource_group_name" {
-  default = "damcey-terraform-rg"
-}
-variable "location" {
-  default = "eastus"
-}
-variable "dns_name_label" {
-  default = "damceyapp-${replace(uuid(), "-", "")}"
+# main.tf
+
+provider "azurerm" {
+  features {}
 }
 
+# Random ID resource to generate a suffix for the DNS name
+resource "random_id" "suffix" {
+  byte_length = 8
+}
+
+# Define the Azure Container Group resource
 resource "azurerm_container_group" "app" {
-  name                = "damceyapp"
+  name                = "${var.container_group_name}-${random_id.suffix.hex}"
   location            = var.location
   resource_group_name = var.resource_group_name
   os_type             = "Linux"
   ip_address_type     = "Public"
-  dns_name_label      = var.dns_name_label
+  dns_name_label      = "${var.container_group_name}-${random_id.suffix.hex}"
 
   container {
     name   = "damcey-app"
@@ -33,4 +33,9 @@ resource "azurerm_container_group" "app" {
   tags = {
     environment = "dev"
   }
+}
+
+# Output the DNS name of the deployed container
+output "container_app_url" {
+  value = azurerm_container_group.app.fqdn
 }
--- terraform-aci/variables.tf
+++ terraform-aci/variables.tf
@@ -1,5 +1,28 @@
+# variables.tf
+
 variable "ghcr_token" {
-  description = "GitHub PAT with read:packages scope"
+  description = "GitHub Container Registry token"
   type        = string
-  sensitive   = true
+}
+
+variable "ghcr_image_url" {
+  description = "The URL of the Docker image in GHCR"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region where resources will be deployed"
+  type        = string
+  default     = "East US"
+}
+
+variable "container_group_name" {
+  description = "Name of the Azure Container Group"
+  type        = string
+  default     = "damceyapp"
 }
